a851a53e9a75cb21bb18f37a41aba86223a812fa
Anurag <77309809+kr-anurag@users.noreply.github.com>
2021-10-31 14:47:00 +0000

    feat: fixed the github icon and added prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
 
     colors: {
-    white: '#fff',
-    primary: '#ED5545',
-    grey: '#202631'
-    }
+      white: "#fff",
+      primary: "#ED5545",
+      grey: "#202631",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
